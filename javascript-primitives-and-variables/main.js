d538846441a1897e3daeff05cee019e300bafd8e
Brian Bozigian <bbozigian@outlook.com>
2021-05-10 10:36:14 -0700

    completed javascript-primitives-and-variables

---
+++ javascript-primitives-and-variables/main.js
@@ -0,0 +1,20 @@
+var fullName = 'Brian Alexander Bozigian';
+console.log('value of Fullname is:', fullName);
+
+var isCool = true;
+console.log('value of isCool is:', isCool);
+
+var totalPets = 0;
+console.log('value of totalPets is:', totalPets);
+
+console.log('type of fullName:', typeof fullName);
+console.log('typeof isCool:', typeof isCool);
+console.log('typeof totalPets:', typeof totalPets);
+
+var unknown = null;
+console.log('value of unknown is:', unknown);
+console.log('typeof unknown:', typeof unknown);
+
+var never;
+console.log('value of never:', never);
+console.log('typeof never :', typeof never);
