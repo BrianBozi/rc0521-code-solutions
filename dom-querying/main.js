962d87b03d68304abe39639c1b35e40a421c32d1
Brian Bozigian <bbozigian@outlook.com>
2021-05-17 10:39:53 -0700

    completed dom-querying

---
+++ dom-querying/main.js
@@ -0,0 +1,16 @@
+console.log('hello world');
+
+var h1s = document.querySelector('h1');
+console.log(h1s);
+
+var eyeD = document.querySelector('#explanation');
+console.log(eyeD);
+
+var hint = document.querySelector('.hint');
+console.log(hint);
+
+var allP = document.querySelectorAll('p');
+console.log(allP);
+
+var allExamples = document.querySelectorAll('.example-link');
+console.log(allExamples);
