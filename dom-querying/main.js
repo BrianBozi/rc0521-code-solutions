8acfcbf5a07acb0a214a18e80724db0b39eddf0a
Brian Bozigian <bbozigian@outlook.com>
2021-05-17 10:47:38 -0700

    added the console.dir into the code

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -2,12 +2,15 @@ console.log('hello world');
 
 var h1s = document.querySelector('h1');
 console.log(h1s);
+console.dir(h1s);
 
 var eyeD = document.querySelector('#explanation');
 console.log(eyeD);
+console.dir(eyeD);
 
 var hint = document.querySelector('.hint');
 console.log(hint);
+console.dir(hint);
 
 var allP = document.querySelectorAll('p');
 console.log(allP);
