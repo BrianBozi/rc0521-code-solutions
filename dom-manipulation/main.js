2dd37ed5ba54dddfe5cdd9094e959e66f0a0f855
Brian Bozigian <bbozigian@outlook.com>
2021-05-17 17:14:47 -0700

    completed dom-manipulation

---
+++ dom-manipulation/main.js
@@ -0,0 +1,23 @@
+var number = 0;
+
+var $hotButton = document.querySelector('.hot-button');
+var $clickCount = document.querySelector('.click-count');
+
+$hotButton.addEventListener('click', function (event) {
+  number++;
+  var temp;
+  if (number < 4) {
+    temp = 'cold';
+  } else if (number < 7) {
+    temp = 'cool';
+  } else if (number < 10) {
+    temp = 'warm';
+  } else if (number < 13) {
+    temp = 'hot';
+  } else {
+    temp = 'nuclear';
+  }
+  $hotButton.className = 'hot-button ' + temp;
+  $clickCount.textContent = 'Clicks: ' + number;
+
+});
