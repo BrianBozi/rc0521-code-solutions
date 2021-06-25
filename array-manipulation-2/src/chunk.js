22836ca10820b0ed3d592b544c7e340c7e952e68
Brian Bozigian <bbozigian@outlook.com>
2021-06-25 13:24:08 -0700

    completed array-manipluation-2

--- array-manipulation-2/src/chunk.js
+++ array-manipulation-2/src/chunk.js
@@ -1,11 +1,10 @@
 /* exported chunk */
-function chunk (array, size){
+
+function chunk(array, size) {
   var arr = [];
-
-  for( var i = 0; i < array.length; i++){
-    var sliced  = array[i].slice(0, size)
-    arr += sliced
+  for (var i = 0; i < array.length; i += size) {
+    var arr2 = array.slice(i, i + size);
+    arr.push(arr2);
   }
-
-return arr
+  return arr;
 }
--- array-manipulation-2/src/includes.js
+++ array-manipulation-2/src/includes.js
@@ -1,13 +1,4 @@
-/* exported includes */
-// function includes(array, value){
-//   for (var i = 0; i < array.length; i++){
-//     if( value === array[i]){
-//       return true
-//     } else {
-//       return false
-//     }
-//   }
-// }
+
 
 function includes(array, value){
   var output = false;
