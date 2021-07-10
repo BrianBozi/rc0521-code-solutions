dbcb2eed00bd4745542daf7075546bfeb124f614
Brian Bozigian <bbozigian@outlook.com>
2021-07-10 00:09:36 +0000

    sip refactoring

--- react-hot-button-component/src/hot-button.jsx
+++ react-hot-button-component/src/hot-button.jsx
@@ -1,13 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 
-// function Button (props) {
-//   return (
-//     <button ClassName={classColor} >Click Me!</button>
-//   );
-// }
-
-
 class HotButton extends React.Component {
   constructor(props){
     super(props);
@@ -20,34 +13,50 @@ class HotButton extends React.Component {
     console.log("work")
   }
 
+
+
+  function Class (){
+    if (this.state.click == 0 || this.state.click <= 2) {
+      return (
+        <button className={'three'}>Click ME!</button>
+      )}
+    else if (this.state.click >= 3 && this.state.click <= 5) {
+      return(
+        <button className={'six'} > Click ME!</button>
+      )
+    }
+  }
+
   render (){
 
-    // const Button = <button className={color}>Hot Button</button>;
-    if(this.state.click == 0 ||  this.state.click <= 2){
-      return (
-        <button onClick={() => this.handleClick()} className={'three'}>Click ME!</button>
-      )
-    } else if (this.state.click >= 3 && this.state.click <= 5) {
-      return(
-        <button onClick={() => this.handleClick()} className={'six'} > Click ME!</button>
-      )
-    } else if (this.state.click >= 6 && this.state.click <= 8){
-      return (
-        <button onClick={() => this.handleClick()} className={'nine'} > Click ME!</button>
-    )}
-      else if (this.state.click >= 9 && this.state.click <= 11) {
-        return (
-        <button onClick={() => this.handleClick()} className={'twelve'} > Click ME!</button>
-    )}
-      else if (this.state.click >= 12 && this.state.click <=14) {
-        return (
-        <button onClick={() => this.handleClick()} className={'fifteen'} > Click ME!</button>
-    )}
-    else if (this.state.click >= 15){
-      return (
-        <button onClick={() => this.handleClick()} className={'eighteen'} > Click ME! </button>
-      )
-  }
+  const Button = <button onClick={() => this.handleClick()} className={color}>click Me </button>;
+
+
+  //   if(this.state.click == 0 ||  this.state.click <= 2){
+  //     return (
+  //       <button onClick={() => this.handleClick()} className={'three'}>Click ME!</button>
+  //     )
+  //   } else if (this.state.click >= 3 && this.state.click <= 5) {
+  //     return(
+  //       <button onClick={() => this.handleClick()} className={'six'} > Click ME!</button>
+  //     )
+  //   } else if (this.state.click >= 6 && this.state.click <= 8){
+  //     return (
+  //       <button onClick={() => this.handleClick()} className={'nine'} > Click ME!</button>
+  //   )}
+  //     else if (this.state.click >= 9 && this.state.click <= 11) {
+  //       return (
+  //       <button onClick={() => this.handleClick()} className={'twelve'} > Click ME!</button>
+  //   )}
+  //     else if (this.state.click >= 12 && this.state.click <=14) {
+  //       return (
+  //       <button onClick={() => this.handleClick()} className={'fifteen'} > Click ME!</button>
+  //   )}
+  //   else if (this.state.click >= 15){
+  //     return (
+  //       <button onClick={() => this.handleClick()} className={'eighteen'} > Click ME! </button>
+  //     )
+  // }
 }
 }
 
