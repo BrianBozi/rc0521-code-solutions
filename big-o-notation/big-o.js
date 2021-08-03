512ce74b102bd95eba77b0e14f6a3e171a313109
Brian Bozigian <bbozigian@outlook.com>
2021-08-03 06:39:28 +0000

    fixed calculations

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -25,20 +25,20 @@ function uniqueQuadratic(words) {
     i++                             // 2 * n = O(2n) ~+ O(n)
   ) {
     const word = words[i];          // 2 * n = O(2n) ~= O(n)
-    let isUnique = true;            // 1  1 = O(1)
+    let isUnique = true;            // 1 * n = O(1n)
     for (
       let c = 0;                    // 1 * 1 = O(1)
-      c < i;                        // 1 * n = O(1n) ~= O(n)
-      c++                           // 2 * n = O(2n) ~=O(n)
+      c < i;                        // 1 * n^2 = O(1n^2) ~= O(n^2)
+      c++                           // 2 * n^2 = O(2n^2) ~=O(n^2)
     ) {
-      const comparing = words[c];   // 1 * n = O(1n) ~= O(n)
-      if (comparing === word) {     // 1 * 1 = O(1)
-        isUnique = false;           // 1 * 1 = O(1)
+      const comparing = words[c];   // 2 * n^2 = O(2n^2) ~= O(n^2)
+      if (comparing === word) {     // 1 * n^2 = O(n^2)
+        isUnique = false;           // 1 * n^2 = O(n^2)
       }
     }
-    if (isUnique) {                 // 1 * 1 = O(1)
+    if (isUnique) {                 // 1 * n = O(n)
       unique[unique.length] = word; // 2 * n = O(2n) ~= O(n)
     }
   }
   return unique;                    // 1 * 1 = O(1)
-} // Big O Notation for uniqueQuadratic: O(n)
+} // Big O Notation for uniqueQuadratic: O(n^2)
